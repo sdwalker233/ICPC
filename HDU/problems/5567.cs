43c2fecc1a41acbe635f03325b8a9d90226eb509
shad0w_walker <admin@shad0w-walker.com>
2015-11-24 23:20:11 +0800

    ACM-ICPC

---
+++ HDU/problems/5567.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+            string[] s = new string[110];
+            int[] a = new int[110];
+            int b, c, n, i, j, cnt;
+            while ((line = Console.ReadLine()) != null)
+            {
+                cnt = 0;
+                s = line.Split(' ');
+                n = int.Parse(s[0]);
+                b = int.Parse(s[1]);
+                c = int.Parse(s[2]);
+                line = Console.ReadLine();
+                s = line.Split(' ');
+                for(i=0;i< n; i++)
+                {
+                    a[i] = int.Parse(s[i]);
+                }
+                for(i=0;i< n; i++)
+                {
+                    for(j=i+1;j< n; j++)
+                    {
+                        if (Math.Abs(a[i] - a[j]) % b == c) cnt++;
+                    }
+                }
+                Console.WriteLine(cnt);
+            }
+        }
+    }
+}
---
+++ HDU/problems/5568.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+            string[] s = new string[110];
+            int[] a = new int[110];
+            int n, m, i, j, k;
+            while ((line = Console.ReadLine()) != null)
+            {
+                BigInteger[,] dp = new BigInteger[105, 105];
+                s = line.Split(' ');
+                n = int.Parse(s[0]);
+                m = int.Parse(s[1]);
+                line = Console.ReadLine();
+                s = line.Split(' ');
+                for (i = 0; i < n; i++)
+                {
+                    a[i] = int.Parse(s[i]);
+                    dp[i,1] = 1;
+                }
+                for(i=2;i<= m; i++)
+                {
+                    for(j=i-1;j< n; j++)
+                    {
+                        dp[j, i] = 0;
+                        for(k=0;k< j; k++)
+                        {
+                            if (a[k] < a[j])
+                            {
+                                dp[j, i] = dp[j, i] + dp[k, i - 1];
+                            }
+                        }
+                    }
+                }
+                BigInteger ans = 0;
+                for(i=0;i< n; i++)
+                {
+                    ans = ans + dp[i, m];
+                }
+                Console.WriteLine(ans);
+            }
+        }
+    }
+}
